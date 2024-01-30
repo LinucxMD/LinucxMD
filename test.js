7320d5ccaf0e5b3827fe92bc05d3bf7895c847a5
Dear NPC :3 <158158782+LinucxMD@users.noreply.github.com>
2024-01-30 18:44:07 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-console.log('Done', file)
+<a href="https://github.com/CupMenolakMcy"><img src="https://github-readme-stats.vercel.app/api?username=HindiaFtNpc&theme=tokyonight&show_icons=true" /></a>
